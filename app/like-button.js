1e2ff508c2e721f0900ad8feada260b2e256a935
aln4739 <henryy20022@gmail.com>
2024-01-23 22:02:58 -0500

    force button to be a client component, while everything else is a server component by default

---
+++ app/like-button.js
@@ -0,0 +1,12 @@
+"use client"; // opt into client components, default is server components to improve performance
+
+import { useState } from "react";
+
+export default function LikeButton() {
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+  return <button onClick={handleClick}>Like ({likes})</button>;
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import LikeButton from "./like-button";
 
 // destructuring props (aka custom arguments)
 function Header({ title }) {
-  return <h1>{`${title} - React`}</h1>;
+  return <h1>{`${title} - Next`}</h1>;
 }
 
 export default function HomePage() {
@@ -10,11 +10,6 @@ export default function HomePage() {
   // React hook to manage state
   // Learn more: https://react.dev/learn/adding-interactivity and https://react.dev/learn/managing-state
   // 'likes' is the number of likes, 'setLikes' is the update function, and the number of likes start at 0.
-  const [likes, setLikes] = useState(0);
-
-  function handleClick() {
-    setLikes(likes + 1);
-  }
 
   return (
     <div>
@@ -25,7 +20,7 @@ export default function HomePage() {
         ))}
       </ul>
       <Header title="React 😎" />
-      <button onClick={handleClick}>Like({likes})</button>
+      <LikeButton />
     </div>
   );
 }
